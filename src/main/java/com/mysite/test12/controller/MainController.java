492b624eb4cd875c47158e33bcd8ab625dafb8e4
k-haechan <gibbm1127@naver.com>
2025-06-30 11:33:27 +0900

    DockerFile

--- src/main/java/com/mysite/test12/controller/MainController.java
+++ src/main/java/com/mysite/test12/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.mysite.controller;
+package com.mysite.test12.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
